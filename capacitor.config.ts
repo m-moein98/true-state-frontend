0db0c26ae884d98714e70cdd7474a8199f7f9559
Mohammadmahdi Moeinzade <moein1475963.mmz@gmail.com>
2022-11-12 11:54:41 +0330

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'true-state',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
